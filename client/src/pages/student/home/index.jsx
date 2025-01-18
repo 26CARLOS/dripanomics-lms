9854869b5a02957a6d462fe47784ec440b5cb554
CARLOSJUMA <u22602047@tuks.co.za>
2025-01-19 01:07:08 +0200

    2nd test commit

--- client/src/pages/student/home/index.jsx
+++ client/src/pages/student/home/index.jsx
@@ -1,6 +1,4 @@
-import { Button } from "@/components/ui/button";
-
-
+import banner from "../../../../public/banner.jpeg"
 function StudentHomePage() {
 
 
@@ -10,9 +8,12 @@ function StudentHomePage() {
             <div className="lg:w-1/2 lg:pr-12 ">
             <h1 className="text-6xl font-bold mb-4">Enriching the lives of others through education.</h1>
             <p className="text-xl">Want better results? Get Started with us.</p>
+
+            </div>
+            <div className="lg:w-full mb-8 lg:mb-0">
+            <img src={banner} alt="homepage" className="w-full h-full object-cover" />
             </div>
         </section>
-
     </div>  );
 }
 
