385746a7ed16543c65ba939cc886c1b2e1e4e232
CARLOSJUMA <u22602047@tuks.co.za>
2025-01-25 02:38:04 +0200

    stdnt courses model update

--- server/models/StudentCourses.js
+++ server/models/StudentCourses.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
-// Create the schema
-const StudentCoursesSchema = new mongoose.Schema({
-    userId: String,
-    courses: [
-        {
-            courseId: String,
-            title: String,
-            instructorId: String,
-            instructorName: String,
-            dateOfPurchase: Date,
-            courseImage: String
-        }
-    ]
+const studentCoursesSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    courses: [{
+        courseId: String,
+        title: String,
+        instructorId: String,
+        instructorName: String,
+        dateOfPurchase: Date,
+        courseImage: String
+    }]
 });
 
-// Export the model only if it hasn't been compiled yet
-module.exports = mongoose.models.StudentCourses || mongoose.model('StudentCourses', StudentCoursesSchema);
+module.exports = mongoose.models.StudentCourses || mongoose.model('StudentCourses', studentCoursesSchema);
